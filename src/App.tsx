6eefb6267c99e0a242253d4445386ed0c3d18165
Albert <albertfrancisco124@gmail.com>
2025-07-29 17:45:22 +0800

    feat: add MapLayerButton component and integrate it into App; update ChatPanel layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,21 @@
 // import viteLogo from '/vite.svg'
 
 import ChatPanel from "./features/chat/components/ChatPanel";
+import MapLayerButton from "./features/map/components/MapLayerButton";
 import MapView from "./features/map/components/MapView";
 
 function App() {
   return (
-    <div className="relative overflow-clip">
+    <div className="absolute inset-0">
+      <div className="fixed flex max-md:flex-col-reverse flex-row max-md:h-80 max-md:w-full md:left-4 bottom-4 md:top-4 rounded-lg z-50">
+        <div className="flex flex-col grow max-md:w-full max-lg:w-80 w-96 bg-white shadow-lg">
+          <ChatPanel />
+        </div>
+        <div className="max-md:py-4 px-4">
+          <MapLayerButton active />
+        </div>
+      </div>
       <MapView />
-      <ChatPanel />
     </div>
   );
 }
--- src/features/chat/components/ChatPanel.tsx
+++ src/features/chat/components/ChatPanel.tsx
@@ -19,16 +19,11 @@ export default function ChatPanel() {
 
   return (
     <>
-      <div className="fixed flex flex-col max-md:w-full max-lg:w-80 w-96 max-md:h-80 justify-between md:left-4 bottom-4 md:top-4 rounded-lg z-50 bg-white shadow-lg">
-        <div className="grow">
-          <ChatConversation />
-        </div>
-        <div className="w-full py-4 max-w-2xl">
-          <ChatInput
-            onSend={handleSend}
-            isLoading={sendingStatus == "sending"}
-          />
-        </div>
+      <div className="grow">
+        <ChatConversation />
+      </div>
+      <div className="w-full py-4">
+        <ChatInput onSend={handleSend} isLoading={sendingStatus == "sending"} />
       </div>
     </>
   );
