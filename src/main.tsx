90c7e8ee735c67ffb76f8a5b884dbd37cf5df213
Albert Francisco <albert.francisco@asti.dost.gov.ph>
2025-09-24 13:31:18 +0800

    feat: initialize React Router

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,20 @@ import "./index.css";
 import App from "./App.tsx";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.ts";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </StrictMode>
 );
